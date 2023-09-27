35a2ce0d705f0faea9746d3787465650b4d2cea9
ramadhancm <ramadhan.maulana@ist.id>
2023-09-27 16:51:08 +0700

    move MainActivity.kt

--- app/src/main/java/com/inbedroom/pokedexter/features/pokemonlist/ui/MainActivity.kt
+++ app/src/main/java/com/inbedroom/pokedexter/features/pokemonlist/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.inbedroom.pokedexter
+package com.inbedroom.pokedexter.features.pokemonlist.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.inbedroom.pokedexter.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
