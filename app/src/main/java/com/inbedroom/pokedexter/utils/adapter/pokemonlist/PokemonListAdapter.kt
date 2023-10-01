b910b1b9661eb9fdd95330d2a352f43928009c79
ramadhancm <ramadhan.maulana@ist.id>
2023-10-01 07:01:00 +0700

    set dynamic color

--- app/src/main/java/com/inbedroom/pokedexter/utils/adapter/pokemonlist/PokemonListAdapter.kt
+++ app/src/main/java/com/inbedroom/pokedexter/utils/adapter/pokemonlist/PokemonListAdapter.kt
@@ -1,14 +1,23 @@
 package com.inbedroom.pokedexter.utils.adapter.pokemonlist
 
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.palette.graphics.Palette
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
+import com.google.android.material.color.utilities.CorePalette
 import com.inbedroom.pokedexter.databinding.ItemPokemonBinding
 import com.inbedroom.pokedexter.databinding.ItemPokemonGridBinding
+import kotlin.math.log
 
 class PokemonListAdapter(
     private val layoutManager: GridLayoutManager? = null,
@@ -46,7 +55,41 @@ class PokemonListAdapter(
         fun bind(item: PokemonModel) {
             binding.root.setOnClickListener(null)
 
-            Glide.with(binding.ivSprite).load(item.sprite).into(binding.ivSprite)
+            Glide.with(binding.ivSprite)
+                .asBitmap()
+                .load(item.sprite)
+                .into(object : CustomTarget<Bitmap>() {
+                    override fun onResourceReady(
+                        resource: Bitmap,
+                        transition: Transition<in Bitmap>?
+                    ) {
+                        Palette.from(resource).generate {
+                            val lightTextColor = it?.lightVibrantSwatch?.titleTextColor
+                            val lightVibrantRgb = it?.lightVibrantSwatch?.rgb
+
+                            it?.dominantSwatch.let { color ->
+                                val (textColor, bgColor) = if (color?.rgb != null && color.rgb < -15100000) {
+                                    lightTextColor to lightVibrantRgb
+                                } else {
+                                    color?.titleTextColor to color?.rgb
+                                }
+
+                                textColor?.let { txtColor ->
+                                    binding.tvName.setTextColor(txtColor)
+                                    binding.tvPokedexEntry.setTextColor(txtColor)
+                                }
+                                bgColor?.let { backgroundColor ->
+                                    binding.root.setCardBackgroundColor(backgroundColor)
+                                }
+                            }
+                        }
+                        binding.ivSprite.setImageBitmap(resource)
+                    }
+
+                    override fun onLoadCleared(placeholder: Drawable?) {
+
+                    }
+                })
 
             binding.tvName.text = item.name
             binding.tvPokedexEntry.text = item.id.toString()
